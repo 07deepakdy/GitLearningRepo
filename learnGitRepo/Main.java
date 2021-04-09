ed18495952419ee077a956705dceec391eaf5d28
07deepakdy <07deepakdy@gmail.com>
2021-04-09 22:58:47 +0530

    new update Main file

---
+++ learnGitRepo/Main.java
@@ -0,0 +1,11 @@
+package com.internshala.javatutorial;
+
+public class Main {
+
+public static void main(String[] args) {
+	// write your code here\
+    System.out.println("Hello Aatish what's about ur besty ?");
+    System.out.println(5*5);
+	System.out.println(45-87765);
+}
+}
