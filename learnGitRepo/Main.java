3d1c184d3afab584b902c367cc577571d1ca250c
07deepakdy <07deepakdy@gmail.com>
2021-04-10 12:00:56 +0530

    dev branch chamge 1

--- learnGitRepo/Main.java
+++ learnGitRepo/Main.java
@@ -5,6 +5,7 @@ public class Main {
 public static void main(String[] args) {
 	// write your code here\
     System.out.println("Hello Aatish what's about ur besty ?");
+    System.out.println("hello deepak");
     System.out.println(5*5);
 	System.out.println(45-87765);
 }
